19a16f212d82bf065ef4c96a312f75fd35cf024c
shamseer81 <shamseer81@gmail.com>
2018-06-28 19:50:13 -0400

    initial push

---
+++ src/main/java/com/employee/employeeSpace/EmployeeSpaceApplication.java
@@ -0,0 +1,12 @@
+package com.employee.employeeSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpaceApplication.class, args);
+	}
+}
